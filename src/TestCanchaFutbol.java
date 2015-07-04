f92653c5292648788b0f9c909cef5d53c5324dd0
Sandra Moreno <smoreno@MacBook-Air-de-Sandra.local>
2015-07-04 14:53:42 -0400

    terminado objeto horas

--- src/TestCanchaFutbol.java
+++ src/TestCanchaFutbol.java
@@ -42,5 +42,67 @@ public class TestCanchaFutbol {
 		reserva.asignarHoraDeInicio(6);
 		assertEquals(6,reserva.obtenerHoraInicio());
 	}
+	/*
+	@Test
+	public void testHoraInicioDeReservaDebeSerMayora5_59AM()
+	{
+		Reserva reserva = new Reserva();
+		reserva.asignarHoraDeInicio(6);
+		assertEquals(6,reserva.obtenerHoraInicio());
+	}
+	*/
+	//HORAS DE RESERVA
+	@Test
+	public void testHoradeReservaDeveSerMayor5()
+	{
+		
+		try {
+			HoraReserva horaReserva = new HoraReserva(5,0);
+			fail();
+		} catch (Exception e) {
+			assertEquals("No se aceptan horas de reservas menores a 6:00",e.getMessage());
+		}
+		
+	}
+	
+	
+	@Test
+	public void testHoradeReservaDeveSerMenorA24()
+	{
+		
+		try {
+			HoraReserva horaReserva = new HoraReserva(24,0);
+			fail();
+		} catch (Exception e) {
+			assertEquals("No se aceptan horas de reservas mayores a 23:00",e.getMessage());
+		}
+		
+	}
+	@Test
+	public void testHoraReservaDebeSerAsignada()
+	{
+		HoraReserva horaReserva = new HoraReserva(7,0);
+		assertEquals(7,horaReserva.obtenerHora());
+	}
+	
+	//Minutos de reserva
+	@Test
+	public void testMinutosDeReservaDebenSer0()
+	{
+		try {
+			HoraReserva horaReserva = new HoraReserva(7,3);
+			fail();
+		} catch (Exception e) {
+			assertEquals("Minuto de reserva debe ser 0 o 30",e.getMessage());
+		}
+	}
+
+	@Test
+	public void testMinutosDeReservaDebenSer30()
+	{
+		
+			HoraReserva horaReserva = new HoraReserva(7,30);
+			assertEquals(30,horaReserva.obtenerMinuto());
+	}
 
 }
